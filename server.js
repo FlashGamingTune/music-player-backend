57678face0cb252b669d5cba15b827ea6d7e6d6c
Saad Mohammed <saadmohammed.kv333@gmail.com>
2026-02-22 16:37:16 +0530

    Removed Unused /stream Route (backend Cleanup)

--- server.js
+++ server.js
@@ -90,44 +90,6 @@ app.get("/songs", (req, res) => {
     }
 });
 
-// Stream song with range support
-app.get("/stream/:filename", (req, res) => {
-    const filePath = path.join(uploadFolder, req.params.filename);
-
-    if (!fs.existsSync(filePath)) {
-        return res.status(404).send("File not found");
-    }
-
-    const stat = fs.statSync(filePath);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-
-    if (range) {
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-        const chunkSize = end - start + 1;
-        const file = fs.createReadStream(filePath, { start, end });
-
-        res.writeHead(206, {
-            "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-            "Accept-Ranges": "bytes",
-            "Content-Length": chunkSize,
-            "Content-Type": "audio/mpeg"
-        });
-
-        file.pipe(res);
-    } else {
-        res.writeHead(200, {
-            "Content-Length": fileSize,
-            "Content-Type": "audio/mpeg"
-        });
-
-        fs.createReadStream(filePath).pipe(res);
-    }
-});
-
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
